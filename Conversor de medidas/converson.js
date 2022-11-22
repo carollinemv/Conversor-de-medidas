7a104ee82fd65a0ed70806ff38416264d6e610a4
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2022-11-22 15:48:19 -0300

    Add files via upload

--- Conversor de medidas/converson.js
+++ Conversor de medidas/converson.js
@@ -14,17 +14,24 @@ const unidade = prompt(
 switch (unidade){
     case "1":
         alert("Resultado: " + medida + "m = " + medida * 1000 + "mm")
+        break
     case "2":
         alert("Resultado: " + medida + "m = " + medida * 100 + "cm")
+        break
     case"3":
-        alert("Resultado: " + medida + "m = " + medida * 10 + "dm") 
+        alert("Resultado: " + medida + "m = " + medida * 10 + "dm")
+        break 
     case "4":
-        alert("Resultado: " + medida + "m = " + medida / 10 + "dam")     
+        alert("Resultado: " + medida + "m = " + medida / 10 + "dam")
+        break     
     case"5":
-        alert("Resultado: " + medida + "m = " + medida / 100 + "hm") 
+        alert("Resultado: " + medida + "m = " + medida / 100 + "hm")
+        break 
     case"6":
         alert("Resultado: " + medida + "m = " + medida / 1000 + "km")
-    default("Opção inválida.")             
+        break
+    default:
+       alert("Opção inválida.")             
 }
 
 
